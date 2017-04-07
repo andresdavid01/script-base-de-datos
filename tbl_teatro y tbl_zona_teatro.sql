57933c00a6ca79d3a65c5305302bde53ec6902ae
andresdavid01 <andresdavid.garcia@ucp.edu.co>
2017-04-07 14:33:18 -0500

    Add files via upload

---
+++ tbl_teatro y tbl_zona_teatro.sql
@@ -0,0 +1,27 @@
+﻿-- Table: administrador.tbl_teatro
+
+-- DROP TABLE administrador.tbl_teatro;
+
+CREATE TABLE administrador.tbl_teatro
+(
+  id_teatro character varying NOT NULL,
+  nombre_teatro character varying NOT NULL,
+  id_zona_teatro character varying NOT NULL,
+  CONSTRAINT id_teatro PRIMARY KEY (id_teatro),
+  CONSTRAINT id_zona_teatro FOREIGN KEY (id_zona_teatro)
+      REFERENCES administrador.tbl_zona_teatro (id_zona_teatro) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
+CREATE TABLE administrador.tbl_zona_teatro
+(
+  id_zona_teatro character varying NOT NULL,
+  nombre_zona_teatro character varying NOT NULL,
+  descripcion_zona_teatro character varying NOT NULL,
+  CONSTRAINT id_zona_teatro PRIMARY KEY (id_zona_teatro)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE administrador.tbl_teatro
+  OWNER TO postgres;
